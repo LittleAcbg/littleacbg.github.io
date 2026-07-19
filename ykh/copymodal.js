04d3b8e397383dedb9fd8e7a0ef002b081b7de7a
LittleAcbg <sh_ljj2012@163.com>
2026-07-19 09:09:37 +0800

    Add files via upload

--- ykh/copymodal.js
+++ ykh/copymodal.js
@@ -5,8 +5,13 @@ function letUserCopy(str) {
 }
 
 // 获取 <span> 元素，设置关闭按钮的点击事件监听器以关闭模态框
+
 document.getElementsByClassName("close")[0].onclick = function() { 
   document.getElementById("copyModal").style.display = "none";
+}
+
+document.getElementsByClassName("close")[1].onclick = function() { 
+  document.getElementById("importModal").style.display = "none";
 }
 
 // 点击窗口之外的区域也可以关闭模态框（可选）
@@ -15,16 +20,15 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  modal = document.getElementById("importModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 // 复制代码到剪贴板的功能函数
-function copyCode() {
-  var code = document.getElementById("copyModalTextarea"); // 获取要复制的代码元素
-  var range = document.createRange(); // 创建一个范围对象（Range）对象用于复制文本内容。 
-  range.selectNode(code); // 选择要复制的节点（这里是代码块） 
-  window.getSelection().removeAllRanges(); // 清空之前的选中区域
-  window.getSelection().addRange(range); // 将选中的区域添加到剪贴板
-  document.execCommand("copy"); // 执行复制命令
-  window.getSelection().removeAllRanges(); // 清空选中区域
+document.getElementById('copyButton').addEventListener('click', async function() {
+  var code = document.getElementById("copyModalTextarea");
+  await navigator.clipboard.writeText(code.innerHTML);
   alert("Sucessfully copied!");
-}
+});
